d5f7bb8578867f6372adba861e2947235f47be6b
Samuel  adikah Mutesh <samsmollett50@gmail.com>
2025-11-20 19:38:09 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -72,15 +72,22 @@ DATABASES = {
 # ===================== REST FRAMEWORK =====================
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
+
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.OrderingFilter',
     ],
 }
+
 
 # ===================== JWT SETTINGS =====================
 SIMPLE_JWT = {
